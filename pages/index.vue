a6b9c1623480f5bac70294e7c81be2b4bd7c7515
Naoto Kaneko <naoty.k@gmail.com>
2019-03-19 21:15:40 +0900

    Show tasks

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,52 @@
 <template>
-  <h1>Hello, nuxt!</h1>
+  <v-container fluid>
+    <v-layout>
+      <v-flex pa-2>
+        <h1>TODO</h1>
+        <v-divider></v-divider>
+        <v-layout column mt-2>
+          <v-flex mb-2>
+            <v-card>
+              <v-card-title>
+                <h2>Task 1</h2>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+          <v-flex mb-2>
+            <v-card>
+              <v-card-title>
+                <h2>Task 2</h2>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex pa-2>
+        <h1>DOING</h1>
+        <v-divider></v-divider>
+        <v-layout column mt-2>
+          <v-flex mb-2>
+            <v-card>
+              <v-card-title>
+                <h2>Task 3</h2>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+      <v-flex pa-2>
+        <h1>DONE</h1>
+        <v-divider></v-divider>
+        <v-layout column mt-2>
+          <v-flex mb-2>
+            <v-card>
+              <v-card-title>
+                <h2>Task 4</h2>
+              </v-card-title>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
