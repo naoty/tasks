fb01b8f250c1ecbc7364f076ef4f558ae0f5485e
Naoto Kaneko <naoty.k@gmail.com>
2019-03-21 21:44:19 +0900

    Make tasks draggable with vuedraggable

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,36 @@
 <template>
   <v-container fluid>
     <v-layout>
-      <v-flex v-for="status in $store.state.statuses" :key="status.id" pa-2>
+      <v-flex v-for="(status, statusId) in $store.state.statuses" :key="statusId" pa-2>
         <h1>{{ status.name }}</h1>
         <v-divider></v-divider>
         <v-layout column mt-2>
-          <v-flex
-            v-for="task in $store.state.tasks.filter(task => task.status === status.id)"
-            :key="task.id"
-            mb-2
-          >
-            <v-card>
-              <v-card-title>
-                <h2>{{ task.title }}</h2>
-              </v-card-title>
-            </v-card>
-          </v-flex>
+          <draggable group="status.id">
+            <v-flex v-for="(task, taskId) in tasksWithStatus(status)" :key="taskId" mb-2>
+              <v-card>
+                <v-card-title>
+                  <h2>{{ task.title }}</h2>
+                </v-card-title>
+              </v-card>
+            </v-flex>
+          </draggable>
         </v-layout>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+import draggable from "vuedraggable";
+
+export default {
+  components: {
+    draggable
+  },
+  methods: {
+    tasksWithStatus: function(status) {
+      return status.tasks.map(taskId => this.$store.state.tasks[taskId]);
+    }
+  }
+};
+</script>
--- store/index.js
+++ store/index.js
@@ -3,17 +3,17 @@ import Vuex from "vuex";
 export default function () {
   return new Vuex.Store({
     state: () => ({
-      statuses: [
-        { id: "1", name: "TODO" },
-        { id: "2", name: "DOING" },
-        { id: "3", name: "DONE" }
-      ],
-      tasks: [
-        { id: "1", title: "Task 1", status: "1" },
-        { id: "2", title: "Task 2", status: "1" },
-        { id: "3", title: "Task 3", status: "2" },
-        { id: "4", title: "Task 4", status: "3" }
-      ]
+      statuses: {
+        "1": { id: "1", name: "TODO", tasks: ["1", "2"] },
+        "2": { id: "2", name: "DOING", tasks: ["3"] },
+        "3": { id: "3", name: "DONE", tasks: ["4"] }
+      },
+      tasks: {
+        "1": { id: "1", title: "Task 1" },
+        "2": { id: "2", title: "Task 2" },
+        "3": { id: "3", title: "Task 3" },
+        "4": { id: "4", title: "Task 4" }
+      }
     })
   });
 }
