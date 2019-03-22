d20a023bbd0aaf38e546b107b1ebfe1b2ab02519
Naoto Kaneko <naoty.k@gmail.com>
2019-03-22 19:17:07 +0900

    Show dialog when clicking button

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,13 @@
       <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title>Tasks</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn depressed>add</v-btn>
+      <v-dialog>
+        <template v-slot:activator="{ on }">
+          <v-btn depressed v-on="on">add</v-btn>
+        </template>
+
+        <h1>Hello, v-dialog!</h1>
+      </v-dialog>
     </v-toolbar>
     <v-content>
       <nuxt/>
