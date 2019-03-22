6e54d211d40a794ff28fbbbadfb61a7e9760eecf
Naoto Kaneko <naoty.k@gmail.com>
2019-03-22 18:02:31 +0900

    Add a button to add a task

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,8 @@
     <v-toolbar>
       <v-toolbar-side-icon></v-toolbar-side-icon>
       <v-toolbar-title>Tasks</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn depressed>add</v-btn>
     </v-toolbar>
     <v-content>
       <nuxt/>
