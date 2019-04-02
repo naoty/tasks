d95ca7bac5d33d8bf8a3775e551633da25733009
Naoto Kaneko <naoty.k@gmail.com>
2019-04-02 15:05:45 +0900

    Create tasks table

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,5 +1,14 @@
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE TABLE `tasks` (
+`task_id` BIGINT (20) NOT NULL AUTO_INCREMENT,
+`title` VARCHAR (255) NOT NULL,
+`status_id` BIGINT (20) DEFAULT NULL,
+PRIMARY KEY (`task_id`),
+INDEX `index_tasks_on_status_id` (`status_id`),
+INDEX `tasks_ibfk_1` (`status_id`),
+CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`status_id`) ON DELETE SET NULL
+) ENGINE = InnoDB, DEFAULT CHARACTER SET = utf8mb4;
 CREATE TABLE `statuses` (
 `status_id` BIGINT (20) NOT NULL AUTO_INCREMENT,
 `name` VARCHAR (255) NOT NULL,
