04ccba6335456ff3e37eba0563c53f326ec05522
Naoto Kaneko <naoty.k@gmail.com>
2019-04-11 11:26:43 +0900

    Remove unused json tags

--- backend/model/status.go
+++ backend/model/status.go
@@ -2,8 +2,8 @@ package model
 
 // Status is the status of tasks.
 type Status struct {
-	StatusID string `json:"id"`
-	Name     string `json:"name"`
-	Position int    `json:"position"`
-	Tasks    []Task `json:"tasks"`
+	StatusID string
+	Name     string
+	Position int
+	Tasks    []Task
 }
--- backend/model/task.go
+++ backend/model/task.go
@@ -2,7 +2,7 @@ package model
 
 // Task represents a task.
 type Task struct {
-	TaskID   string `json:"id"`
-	Title    string `json:"title"`
-	StatusID string `json:"statusId"`
+	TaskID   string
+	Title    string
+	StatusID string
 }
