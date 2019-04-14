df61676226a93e189562871d45d648853b0ddac6
Naoto Kaneko <naoty.k@gmail.com>
2019-04-14 22:12:18 +0900

    Insert tasks into db in mutation resolver

--- backend/resolver/mutation.go
+++ backend/resolver/mutation.go
@@ -11,5 +11,35 @@ type mutationResolver struct {
 }
 
 func (r *mutationResolver) CreateTask(ctx context.Context, title string) (*model.Task, error) {
-	panic("not implemented")
+	statusRows, err := r.DB.Query("SELECT status_id FROM statuses ORDER BY position ASC LIMIT 1")
+	if err != nil {
+		return nil, err
+	}
+
+	defer statusRows.Close()
+
+	var initialStatusID int
+	statusRows.Next()
+	err = statusRows.Scan(&initialStatusID)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = r.DB.Exec("INSERT INTO tasks (title, status_id) VALUES (?, ?)", title, initialStatusID)
+	if err != nil {
+		return nil, err
+	}
+
+	taskRows, err := r.DB.Query("SELECT * FROM tasks ORDER BY task_id DESC LIMIT 1")
+	if err != nil {
+		return nil, err
+	}
+
+	defer taskRows.Close()
+
+	var task model.Task
+	taskRows.Next()
+	err = taskRows.Scan(&task.TaskID, &task.Title, &task.StatusID)
+
+	return &task, err
 }
