b5fe75fea6a7f576a29286281fec0ee79c1f6dbc
Naoto Kaneko <naoty.k@gmail.com>
2019-04-15 13:57:49 +0900

    Implement taskResolver to return a task

--- backend/resolver/task.go
+++ backend/resolver/task.go
@@ -11,5 +11,16 @@ type taskResolver struct {
 }
 
 func (r *taskResolver) Status(ctx context.Context, obj *model.Task) (*model.Status, error) {
-	panic("not implemented")
+	rows, err := r.DB.Query("SELECT * FROM statuses WHERE status_id = ? ORDER BY status_id ASC LIMIT 1", obj.StatusID)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var status model.Status
+	rows.Next()
+	err = rows.Scan(&status.StatusID, &status.Name, &status.Position)
+
+	return &status, err
 }
