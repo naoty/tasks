fbe6a9c542aff5fd62aeb62f8a98e3374748dd14
Naoto Kaneko <naoty.k@gmail.com>
2019-04-19 13:29:35 +0900

    Move schema.graphql into graphql directory

--- backend/gqlgen/generated.go
+++ backend/gqlgen/generated.go
@@ -239,7 +239,7 @@ func (ec *executionContext) introspectType(name string) (*introspection.Type, er
 }
 
 var parsedSchema = gqlparser.MustLoadSchema(
-	&ast.Source{Name: "../../schema.graphql", Input: `type Query {
+	&ast.Source{Name: "../../graphql/schema.graphql", Input: `type Query {
   statuses: [Status!]!
 }
 
